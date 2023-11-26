3354f7829e42fb5e9f8742a4734c5292c5720f4d
MrBadshah69 <mrbadshahbusiness@gmail.com>
2023-11-26 07:02:44 +0500

    Working On Procees

--- Admin_area/Insert_Catagories.php
+++ Admin_area/Insert_Catagories.php
@@ -7,9 +7,16 @@ include("../database.php");
 if (isset($_POST['insert_category'])) {
   $category_title = $_POST['cate_title'];
 
+  // Image 
+  $category_image = $_FILES['cate_image']['name'];
+  
+  // tmp image
+  $tmp_category_image = $_FILES['cate_image']['tmp_name'];
 
 
-  $select_query = "Select * from `catagories` where Category_tittle='$category_title'";
+
+
+  $select_query = "Select * from `catagories` where Category_tittle='$category_title' Category_Image='$category_image'";
 
   $select_result = mysqli_query($conn, $select_query);
   $number = mysqli_num_rows($select_result);
@@ -17,9 +24,10 @@ if (isset($_POST['insert_category'])) {
     echo ("<div class='alert alert-danger'>Category already exists</div>");
   } else {
 
+    // Image Move
+    move_uploaded_file($tmp_category_image, "./Category_Image/$category_image");
 
-
-    $insert_query = "insert into `catagories` (Category_tittle) values ('$category_title')";
+    $insert_query = "insert into `catagories` (Category_tittle,Category_Image) values ('$category_title','$category_image')";
 
     $Result = mysqli_query($conn, $insert_query);
     if ($Result) {
@@ -48,6 +56,9 @@ if (isset($_POST['insert_category'])) {
   <div class="input-group m-3 w-90">
     <span class="input-group-text" id="basic-addon1"><i class="fa-solid fa-receipt"></i></span>
     <input type="text" class="form-control" name="cate_title" placeholder="Insert Catagories" aria-label="Username" aria-describedby="basic-addon1">
+  </div>
+  <div class="input-group m-3 w-90">
+    <input type="file" class="form-control" name="cate_image" placeholder="Insert Catagories Image">
   </div>
   <div class="input-group m-3 p-1">
     <button name="insert_category" class=" border-0 bn5">Insert Catagories
--- Functions/Common_Functions.php
+++ Functions/Common_Functions.php
@@ -1,21 +1,28 @@
 <?php
 include('database.php');
 
-
+// View Products
 function viewproduct()
 {
     global $conn;
-    $select_product_detail = "SELECT * from `products`";
-    $connect_data = mysqli_query($conn, $select_product_detail);
-    while ($drag_row_data = mysqli_fetch_assoc($connect_data)) {
 
-        $Product_image1 = $drag_row_data['Product_image1'];
-        $Product_image2 = $drag_row_data['Product_image2'];
-        $Product_image3 = $drag_row_data['Product_image3'];
-        $Product_title = $drag_row_data['Product_Title'];
-        $Product_Price = $drag_row_data['Product_Price'];
+    if (!isset($_GET['Category'])) {
+        if (!isset($_GET['Brands'])) {
 
-        echo "<div class='p-2 col-lg-3 col-md-6 col-6'>
+
+            $select_product_detail = "SELECT * from `products`";
+            $connect_data = mysqli_query($conn, $select_product_detail);
+            while ($drag_row_data = mysqli_fetch_assoc($connect_data)) {
+
+                $Product_image1 = $drag_row_data['Product_image1'];
+                $Product_image2 = $drag_row_data['Product_image2'];
+                $Product_image3 = $drag_row_data['Product_image3'];
+                $Product_title = $drag_row_data['Product_Title'];
+                $Product_Price = $drag_row_data['Product_Price'];
+                $Category_id = $drag_row_data['Category_id'];
+                $Brands_id = $drag_row_data['Brands_id'];
+
+                echo "<div class='p-2 col-lg-3 col-md-6 col-6'>
                 <div class='card border-0'>
                         <img src='Admin_area/Products_image/$Product_image1' class='card-img-top' alt='Product-img'>
                     <div class='card-body'>
@@ -24,11 +31,48 @@ function viewproduct()
                 </div>
                     </div>
                         </div>";
+            }
+        }
     }
 }
 
+// View unqiue Category
+function viewunqiuecatgories()
+{
+    global $conn;
+    if (isset($_GET['Category'])) {
+        $Category_id = $_GET['Category'];
 
+        $select_product_detail = "SELECT * from `products` where Category_id=$Category_id";
+        $connect_data = mysqli_query($conn, $select_product_detail);
+        $count_row = mysqli_num_rows($connect_data);
+        if ($count_row == 0) {
+            echo "<h1 class='text-center py-5 alert alert-info text-dark mt-2'>Products Not Available</h1>";
+        }
+        while ($drag_row_data = mysqli_fetch_assoc($connect_data)) {
 
+            $Product_image1 = $drag_row_data['Product_image1'];
+            $Product_image2 = $drag_row_data['Product_image2'];
+            $Product_image3 = $drag_row_data['Product_image3'];
+            $Product_title = $drag_row_data['Product_Title'];
+            $Product_Price = $drag_row_data['Product_Price'];
+            $Category_id = $drag_row_data['Category_id'];
+            $Brands_id = $drag_row_data['Brands_id'];
+
+            echo "<div class='p-2 col-lg-3 col-md-6 col-6'>
+                <div class='card border-0'>
+                        <img src='Admin_area/Products_image/$Product_image1' class='card-img-top' alt='Product-img'>
+                    <div class='card-body'>
+                        <h6 class='card-title'>$Product_title</h6>
+                        <span>$Product_Price</span>
+                </div>
+                    </div>
+                        </div>";
+        }
+    }
+}
+
+// View Category
 function viewcatagories()
 {
     global $conn;
@@ -44,6 +88,42 @@ function viewcatagories()
     }
 }
 
+// View unqiue Category
+function viewunqiuebrands()
+{
+    global $conn;
+    if (isset($_GET['Brands'])) {
+        $Brands_id = $_GET['Brands'];
+
+        $select_product_detail = "SELECT * from `products` where Brands_id=$Brands_id";
+        $connect_data = mysqli_query($conn, $select_product_detail);
+        $count_row = mysqli_num_rows($connect_data);
+        if ($count_row == 0) {
+            echo "<h1 class='text-center py-5 alert alert-info text-dark mt-2'>Products Not Available</h1>";
+        }
+        while ($drag_row_data = mysqli_fetch_assoc($connect_data)) {
+
+            $Product_image1 = $drag_row_data['Product_image1'];
+            $Product_image2 = $drag_row_data['Product_image2'];
+            $Product_image3 = $drag_row_data['Product_image3'];
+            $Product_title = $drag_row_data['Product_Title'];
+            $Product_Price = $drag_row_data['Product_Price'];
+            $Brands_id = $drag_row_data['Brands_id'];
+
+            echo "<div class='p-2 col-lg-3 col-md-6 col-6'>
+                <div class='card border-0'>
+                        <img src='Admin_area/Products_image/$Product_image1' class='card-img-top' alt='Product-img'>
+                    <div class='card-body'>
+                        <h6 class='card-title'>$Product_title</h6>
+                        <span>$Product_Price</span>
+                </div>
+                    </div>
+                        </div>";
+        }
+    }
+}
+
+// View Brands
 function viewbrands()
 {
     global $conn;
@@ -56,4 +136,41 @@ function viewbrands()
                             <a class='nav-link' href='Product_page.php?Brands=$Brands_id'>$Brands_tittle</a>
                         </li>";
     }
+}
+
+
+/////// Search Product box
+
+function searchproduct()
+{
+
+    global $conn;
+
+    if (isset($_GET['Search_click'])) {
+        $search_box_vlaue = $_GET['Search_product_box'];
+        
+        $Search_product = "SELECT * from `products` where Product_Keywords like'%$search_box_vlaue%'";
+    $connect_data = mysqli_query($conn, $Search_product);
+    while ($drag_row_data = mysqli_fetch_assoc($connect_data)) {
+        
+        $Product_image1 = $drag_row_data['Product_image1'];
+        $Product_image2 = $drag_row_data['Product_image2'];
+        $Product_image3 = $drag_row_data['Product_image3'];
+        $Product_title = $drag_row_data['Product_Title'];
+        $Product_Price = $drag_row_data['Product_Price'];
+        $Category_id = $drag_row_data['Category_id'];
+        $Brands_id = $drag_row_data['Brands_id'];
+        $Product_Keywords=$drag_row_data['Product_Keywords'];
+        
+        echo "<div class='p-2 col-lg-3 col-md-6 col-6'>
+        <div class='card border-0'>
+        <img src='Admin_area/Products_image/$Product_image1' class='card-img-top' alt='Product-img'>
+        <div class='card-body'>
+        <h6 class='card-title'>$Product_title</h6>
+        <span>$Product_Price</span>
+        </div>
+        </div>
+        </div>";
+    }
+}
 }
